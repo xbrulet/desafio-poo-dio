d1f350c57848ddbef3cd42e44cd9332e306b9ba7
xbrulet <bruletalmeida@gmail.com>
2023-04-26 18:49:58 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
-        curso1.setDescricao("Exemplo de descrição do curso Java");
+        curso1.setDescricao("Exemplo descrição do curso Java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso JavaScript");
-        curso2.setDescricao("Exemplo de descrição do curso JavaScript");
+        curso2.setDescricao("Exemplo descrição do curso JavaScript");
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
-        mentoria.setDescricao("Exemplo de descrição da mentoria de Java");
+        mentoria.setDescricao("Exemplo descrição da mentoria de Java");
         mentoria.setData(LocalDate.now());
 
         /*System.out.println(curso1);
@@ -25,7 +25,7 @@ public class Main {
 
         Bootcamp bootcamp = new Bootcamp();
         bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Exemplo de descrição do Bootcamp Java Developer");
+        bootcamp.setDescricao("Exemplo descrição do Bootcamp Java Developer");
         bootcamp.getConteudos().add(curso1);
         bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(mentoria);
